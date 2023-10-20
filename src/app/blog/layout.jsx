1c4004a564593dfed222ec39608d88ccb46ebb2c
Meiirlan Serikbay <114663479+meiirsse@users.noreply.github.com>
2023-10-20 12:05:03 +0600

    layouts and components

---
+++ src/app/blog/layout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const BlogLayout = ({ children }) => {
+  return (
+    <div>
+      <h1>Hello, this is blog</h1>
+      {children}
+    </div>
+  )
+}
+
+export default Users
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      <div>©2023 SSemeii. All rights reserved.</div>
+      <div></div>
+    </div>
+  )
+}
+
+export default Footer
